3fac99c6fd4743af77d8ea8ea2310812486ff893
Ibsy Matute <ibsy@rocketmail.com>
2022-08-16 16:06:39 -0500

    add user to database on sign up submit

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template
 from app.forms import SignUpForm
+from app.models import User
 
 @app.route('/')
 def index():   # also an endpoint
@@ -17,6 +18,10 @@ def signup():   # also an endpoint
     # if the form is submitted and all the data is valid
     if form.validate_on_submit():
         print('Form has been validated! Hooray!!!')
-        print(form.email.data, form.username.data, form.password.data) 
         # each form field has a data attribute
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        new_user = User(email=email, username=username, password=password)
+        print(f"{new_user.username} has been created.")
     return render_template('signup.html', form=form)
