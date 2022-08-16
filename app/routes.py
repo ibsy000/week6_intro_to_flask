55173cd25a84add77f6e4a92a53d3f199c1c7031
Ibsy Matute <ibsy@rocketmail.com>
2022-08-16 11:47:51 -0500

    change test route to signup

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-def index():
+def index():   # also an endpoint
     user_info = {
         'username': 'brians',
         'email': 'brians@codingtemple.com'
@@ -10,6 +10,6 @@ def index():
     colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
     return render_template('index.html', user=user_info, colors=colors)
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
+@app.route('/signup')
+def signup():   # also an endpoint
+    return render_template('signup.html')
