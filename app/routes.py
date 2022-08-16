61aa195308ede25e75627cf2b7c94e767cb1c691
Ibsy Matute <ibsy@rocketmail.com>
2022-08-16 14:17:32 -0500

    add form submit abilities

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,12 @@ def index():   # also an endpoint
     colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
     return render_template('index.html', user=user_info, colors=colors)
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():   # also an endpoint
     form = SignUpForm() # instantiate SignUpForm class
+    # if the form is submitted and all the data is valid
+    if form.validate_on_submit():
+        print('Form has been validated! Hooray!!!')
+        print(form.email.data, form.username.data, form.password.data) 
+        # each form field has a data attribute
     return render_template('signup.html', form=form)
