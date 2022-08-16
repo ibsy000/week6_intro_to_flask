6b347f34b43798ab683db1e2ed39f5cd5c1c3f4b
Ibsy Matute <ibsy@rocketmail.com>
2022-08-16 14:54:48 -0500

    added config,update app.config and flask_migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask # Flask is a class
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__) # create first instance of class Flask, parameter will always
 # be __name__
-app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config.from_object(Config)
 #   config subclass of a dictionary
+# from_object will go through class attributes
+
+# Create an instance of SQLAlchemy (the ORM) with the Flask Application
+db = SQLAlchemy(app)
+# Create an instance of Migrate which will be our migration engine and pass in the app and SQLAlchemy instance
+migrate = Migrate(app, db)
+
 
 
 from . import routes
