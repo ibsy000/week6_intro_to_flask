110fc2aa96bfdfc38ec025dec05adc49751708d4
Ibsy Matute <ibsy@rocketmail.com>
2022-08-16 15:56:01 -0500

    add __init__ to the User class

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash
 
 
 class User(db.Model):
@@ -8,3 +9,10 @@ class User(db.Model):
     username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) # Coordinated Universal Time (always save it to this time when storing, and convert it later on front end)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # Save the password as the hashed version of the password
+        self.password = generate_password_hash(kwargs['password'])
+        db.session.add(self)
+        db.session.commit()
