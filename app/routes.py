fffc25b4808081ffaa5a5fee6da4132dafd090b3
Ibsy Matute <ibsy@rocketmail.com>
2022-08-17 16:16:00 -0500

    Minor:update nav

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template, redirect, url_for, flash
 from app.forms import SignUpForm, PostForm
-from app.models import User
+from app.models import User, Post
 
 @app.route('/')
 def index():   # also an endpoint
@@ -40,7 +40,19 @@ def signup():   # also an endpoint
 
 
 
-@app.route('/create')
+@app.route('/create', methods=['GET', 'POST'])
 def create():
     form = PostForm()
+
+    if form.validate_on_submit():
+        # Get the data from the form
+        title = form.title.data
+        body = form.body.data
+        # Create new instance of Post with the form data
+        new_post = Post(title=title, body=body, user_id=1)
+        # flash a message saying the post was created
+        flash(f'{new_post.title} has been created.', 'secondary')
+        # Redirect back to home page
+        return redirect(url_for('index'))
+
     return render_template('createpost.html', form=form)
